5b5b6571aab92307382b7742c598393848ec24a0
Tarif Halabi <tarif.halabi@gmail.com>
2020-06-21 21:17:57 -0400

    Add clientBuildTimestamp & serverBuildTimestamp.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AvWx';
+    clientBuildTimestamp: string;
+    serverBuildTimestamp: string;
+    title = 'AvWx';
+
+    ngOnInit() {
+        this.clientBuildTimestamp = environment.buildTimestamp;
+    }
 }
